60e13c26a8ef38614a905bb71a9bf23f0bcfdff7
Lucas Fonseca <lucasrfon@gmail.com>
2022-06-19 19:08:51 -0300

    implementa POST /sign-up

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ import cors from "cors";
 
 const server = express();
 server.use(cors());
+server.use(express.json());
+
+const users = [];
+
+server.post('/sign-up', (req, res) => {
+    users.push(req.body);
+    res.send("OK");
+});
+
+server.listen(5000);
