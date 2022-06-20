656d541e21bff683360768721d51fcf137fcb182
Lucas Fonseca <lucasrfon@gmail.com>
2022-06-20 03:58:24 -0300

    implementa validação de dados

--- server.js
+++ server.js
@@ -9,16 +9,30 @@ const users = [];
 const tweets = [];
 
 server.post('/sign-up', (req, res) => {
-    users.push(req.body);
-    res.send("OK");
+    const badUsername = typeof(req.body.username) !== "string" || req.body.username.trim().length <= 0;
+    const badAvatar = typeof(req.body.avatar) !== "string" || req.body.avatar.slice(0, 8) !== "https://";
+    
+    if(badUsername || badAvatar) {
+        res.status(400).send("Todos os campos são obrigatórios!");
+    } else {
+        users.push(req.body);
+        res.send("OK");
+    }
 });
 
 server.post('/tweets', (req, res) => {
-    function findAvatar(object) {
-        return object.username === req.body.username;
+    const badUsername = typeof(req.body.username) !== "string" || req.body.username.trim().length <= 0;;
+    const badTweet = typeof(req.body.tweet) !== "string" || req.body.tweet.trim().length <= 0;
+
+    if(badUsername || badTweet) {
+        res.status(400).send("Todos os campos são obrigatórios!");
+    } else {
+        function findAvatar(object) {
+            return object.username === req.body.username;
+        }
+        tweets.push({...req.body, "avatar": users.find(findAvatar).avatar});
+        res.send("OK");
     }
-    tweets.push({...req.body, "avatar": users.find(findAvatar).avatar});
-    res.send("OK");
 });
 
 server.get('/tweets', (req, res) => {
